cdda279d1cae9c9412230ac6a96b4ccff6e360d8
Macgie <maciejgier@gmail.com>
2024-06-07 12:47:19 +0200

    adds shooting and improved collision

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from sdl2.sdlttf import *
 
 
 class Position:
-    def __init__(self, start_x, start_y, velocity, prop, r):
+    def __init__(self, start_x, start_y, velocity_x, velocity_y, prop, r):
         self.start_x = start_x
         self.start_y = start_y
-        self.velocity = velocity
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
         self.pos_y = start_y
         self.pos_x = start_x
         self.prop = prop
@@ -18,10 +19,10 @@ class Position:
 
     def is_outside(self):
         if self.prop:
-            if self.pos_x >= 620:
-                self.pos_x = self.start_x
-            if self.pos_y >= 900:
-                self.pos_y = self.start_y
+            if self.pos_x >= 620 or self.pos_x <= 0:
+                self.velocity_x = -self.velocity_x
+            if self.pos_y >= 900 or self.pos_y <= 0:
+                self.velocity_y = -self.velocity_y
 
     def set_pos_x(self, pos_x):
         self.pos_x = pos_x
@@ -32,27 +33,31 @@ class Position:
     def set_pos_y(self, pos_y):
         self.pos_y = pos_y
 
-    def move_on(self, vertical):
-        if vertical:
-            self.set_pos_y(pos_y=self.pos_y + self.velocity)
-            self.ry = self.pos_y + self.r
-        else:
-            self.set_pos_x(pos_x=self.pos_x + self.velocity)
-            self.rx = self.pos_x + self.r
+    def move_on(self):
+        self.set_pos_y(pos_y=self.pos_y + self.velocity_y)
+        self.set_pos_x(pos_x=self.pos_x + self.velocity_x)
+        self.ry = self.pos_y + self.r
+        self.rx = self.pos_x + self.r
 
     def get_pos_y(self):
         return self.pos_y
 
-    def is_colision(self, player_x, player_y):
+    def is_colision_with_meteor(self, player_x, player_y, prop_width, prop_height):
         distance_x_0 = player_x - self.rx
         distance_y_0 = player_y - self.ry
-        distance_x_m = (player_x+58) - self.rx
-        distance_y_m = (player_y+37) - self.ry
+        distance_x_m = (player_x + prop_width) - self.rx
+        distance_y_m = (player_y + prop_height) - self.ry
         distance_0_0 = (distance_x_0 ** 2 + distance_y_0 ** 2) ** 0.5
         distance_x_y = (distance_x_m ** 2 + distance_y_m ** 2) ** 0.5
         distance_0_y = (distance_x_0 ** 2 + distance_y_m ** 2) ** 0.5
         distance_x_0 = (distance_x_m ** 2 + distance_y_0 ** 2) ** 0.5
         return distance_0_0 <= self.r or distance_x_y <= self.r or distance_x_0 <= self.r or distance_0_y <= self.r
+
+    def change_direction_and_speed(self):
+        self.velocity_x = -self.velocity_x
+        self.velocity_y = -self.velocity_y
+
+
 class Game:
     def __init__(self):
         if sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO) != 0:
@@ -62,7 +67,7 @@ class Game:
             100,
             100,
             800,
-            600,
+            800,
             sdl2.SDL_WINDOW_SHOWN
         )
         self.running = True
@@ -71,14 +76,21 @@ class Game:
             -1,
             sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC
         )
-        img_path = 'img'
         self.background = self.loadTexture('img/background.bmp', renderer=self.ren)
         self.ship = self.loadTexture('img/ship.bmp', renderer=self.ren)
         self.meteor = self.loadTexture('img/asteroida.bmp', renderer=self.ren)
         self.playerX = 0
         self.playerY = 0
-        self.velocity = 0
+        self.velocityY = 0
+        self.velocityX = 0
+        self.accelerationX = 0
+        self.accelerationY = 0
         self.lost = False
+        self.shield = True
+        self.p_bullets = []
+
+    def shoot(self):
+        self.p_bullets.append([self.playerX + 29, self.playerY])
 
     def loadTexture(self, filePath, renderer):
         img = sdl2.SDL_LoadBMP(str.encode(filePath))
@@ -102,8 +114,8 @@ class Game:
         sdl2.SDL_RenderCopy(render, texture, None, loc)
 
     def run(self):
-        meteor1 = Position(450, -100, 5, True, 72)
-        meteor2 = Position(200, -100, 5, True, 72)
+        meteor1 = Position(450, 100, 5, 5, True, 72)
+        meteor2 = Position(200, 500, 5, 5, True, 72)
         if not self.background:
             sdl2.SDL_DestroyTexture(self.background)
             sdl2.SDL_DestroyRenderer(self.ren)
@@ -118,29 +130,83 @@ class Game:
                     break
                 if event.type == sdl2.SDL_KEYDOWN:
                     if event.key.keysym.sym == sdl2.SDLK_LEFT:
-                        self.playerX -= 10
+                        self.accelerationX = -0.5
                     elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
-                        self.playerX += 10
+                        self.accelerationX = 0.5
                     elif event.key.keysym.sym == sdl2.SDLK_UP:
-                        self.playerY -= 10
+                        self.accelerationY = -0.5
                     elif event.key.keysym.sym == sdl2.SDLK_DOWN:
-                        self.playerY += 10
+                        self.accelerationY = 0.5
                     elif event.key.keysym.sym == sdl2.SDLK_ESCAPE:
                         self.running = False
                         break
+                    elif event.key.keysym.sym == sdl2.SDLK_SPACE:
+                        self.shoot()
+                    elif event.key.keysym.sym == sdl2.SDLK_r:
+                        self.run()
 
+                if event.type == sdl2.SDL_KEYUP:
+                    if event.key.keysym.sym in [sdl2.SDLK_LEFT, sdl2.SDLK_RIGHT]:
+                        self.accelerationX = 0
+                    if event.key.keysym.sym in [sdl2.SDLK_UP, sdl2.SDLK_DOWN]:
+                        self.accelerationY = 0
+
+            self.velocityX += self.accelerationX
+            self.velocityY += self.accelerationY
+            self.playerX += self.velocityX
+            self.playerY += self.velocityY
+
+            if self.playerX < 0:
+                self.playerX = 0
+                self.velocityX = 0
+            if self.playerX + 58 > 800:
+                self.playerX = 800 - 58
+                self.velocityX = 0
+            if self.playerY < 0:
+                self.playerY = 0
+                self.velocityY = 0
+            if self.playerY + 37 > 600:
+                self.playerY = 600 - 37
+                self.velocityY = 0
+
+            sdl2.SDL_Delay(10)
             meteor1.is_outside()
-            meteor1.move_on(True)
+            meteor1.move_on()
             meteor2.is_outside()
-            meteor2.move_on(True)
-            if meteor2.is_colision(player_x=self.playerX,player_y=self.playerY) or meteor1.is_colision(player_x=self.playerX,player_y=self.playerY):
-                print("KOLIZJA !!!")
+            meteor2.move_on()
+            if meteor1.is_colision_with_meteor(
+                    player_y=meteor2.get_pos_y(), player_x=meteor2.get_pos_x(), prop_width=144, prop_height=130):
+                sdl2.SDL_Delay(15)
+                meteor1.change_direction_and_speed()
 
+            if meteor2.is_colision_with_meteor(player_x=meteor1.get_pos_x(), player_y=meteor1.get_pos_y(),
+                                               prop_width=144, prop_height=130):
+                sdl2.SDL_Delay(15)
+                meteor2.change_direction_and_speed()
+
+            if meteor1.is_colision_with_meteor(player_x=self.playerX, player_y=self.playerY, prop_width=58,
+                                               prop_height=37):
+                self.velocityX = -self.velocityX
+                self.velocityY = -self.velocityY
+                meteor1.change_direction_and_speed()
+            if meteor2.is_colision_with_meteor(player_x=self.playerX, player_y=self.playerY, prop_width=58,
+                                               prop_height=37):
+                self.velocityY = -self.velocityY
+                self.velocityX = -self.velocityX
+                meteor2.change_direction_and_speed()
+            for bullet in self.p_bullets:
+                bullet[0] += 10
+            self.p_bullets = [bullet for bullet in self.p_bullets if bullet[1] > 0]
             sdl2.SDL_RenderClear(self.ren)
             self.renderTexture(self.background, self.ren, 0, 0)
-            self.renderTexture(self.ship, self.ren, self.playerX, self.playerY)
+            self.renderTexture(self.ship, self.ren, int(self.playerX), int(self.playerY))
             self.renderTexture(self.meteor, self.ren, meteor1.get_pos_x(), meteor1.get_pos_y())
             self.renderTexture(self.meteor, self.ren, meteor2.get_pos_x(), meteor2.get_pos_y())
+            sdl2.SDL_SetRenderDrawColor(self.ren, 255, 0, 0, 255)
+            sdl2.SDL_SetRenderDrawColor(self.ren, 255, 0, 0, 255)
+            for bullet in self.p_bullets:
+                sdl2.SDL_RenderDrawLine(self.ren, int(bullet[0]), int(bullet[1]), int(bullet[0]) + 15, int(bullet[1]))
+
             sdl2.SDL_RenderPresent(self.ren)
         sdl2.SDL_DestroyTexture(self.background)
         sdl2.SDL_DestroyTexture(self.ship)
